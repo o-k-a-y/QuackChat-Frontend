610fe354e6ef92208ffbb5f6ff792edda68a8944
o-k-a-y <btunney@ramapo.edu>
2020-04-15 01:37:06 -0400

    Making DAO for storing friend data

--- QuackChat/app/src/main/java/edu/ramapo/btunney/quackchat/CameraActivity.kt
+++ QuackChat/app/src/main/java/edu/ramapo/btunney/quackchat/CameraActivity.kt
@@ -5,9 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.TextView
+import edu.ramapo.btunney.quackchat.dao.AppDatabase
+import edu.ramapo.btunney.quackchat.dao.UserDao
+import edu.ramapo.btunney.quackchat.dao.database
 import edu.ramapo.btunney.quackchat.networking.NetworkCallback
 import edu.ramapo.btunney.quackchat.networking.NetworkRequester
 import edu.ramapo.btunney.quackchat.networking.ServerRoutes
+import kotlinx.android.synthetic.main.activity_camera.*
+import org.json.JSONArray
 
 class CameraActivity : AppCompatActivity() {
 
@@ -15,7 +21,15 @@ class CameraActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
 
+
+//        val test: database = database()
+//        val dao = test.userDao()
+
+
+        // TODO: ALL OF THIS SHOULD BE MOVED TO THE FRIENDS ACTIVITY WITH A CUSTOM VIEW FOR EACH FRIEND
+        val activityRef = this
         // Load friends
+//        var friends: JSONArray? = null
         NetworkRequester.fetchFriends(ServerRoutes.GET_FRIENDS, object: NetworkCallback {
             override fun onFailure(failureCode: NetworkCallback.FailureCode) {
                 TODO("Not yet implemented")
@@ -24,16 +38,23 @@ class CameraActivity : AppCompatActivity() {
             override fun onSuccess(data: Any?) {
 //                TODO("Not yet implemented")
                 Log.d("friend call", "success")
+//                println(data)
 
-                // TODO: handle data
-                // TODO
+                runOnUiThread {
+                    Runnable {
+//                        var friends = JSONArray(data)
+                        var newView: TextView = TextView(activityRef)
+                        // TOOD: no
+                        newView.text = "hello"
+                        activityRef.friendListScrollView.addView(newView)
+                        println(data)
+                    }.run()
+                }
+
+
             }
 
         })
-
-
-        // Check if auth'd
-
     }
 
 
